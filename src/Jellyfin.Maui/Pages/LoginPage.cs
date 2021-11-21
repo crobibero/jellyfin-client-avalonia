1d1c5d2c7e1c29a09aa75c16d0dc6b7c7bbc389b
Cody Robibero <cody@robibe.ro>
2021-11-21 12:07:05 -0700

    Make login page more fluent

--- src/Jellyfin.Maui/Pages/LoginPage.cs
+++ src/Jellyfin.Maui/Pages/LoginPage.cs
@@ -1,5 +1,6 @@
 ﻿using Jellyfin.Maui.ViewModels;
 using CommunityToolkit.Maui.Markup;
+using Microsoft.Maui;
 
 namespace Jellyfin.Maui.Pages;
 
@@ -20,29 +21,69 @@ public class LoginPage : BaseContentPage<LoginViewModel>
     /// <inheritdoc />
     protected override void InitializeLayout()
     {
-        Content = new StackLayout
+        Content = new ScrollView
         {
-            Padding = 16,
-            Children =
+            Content = new Grid
             {
-                new Label{ Text = "Server URL"},
-                new Entry{HorizontalOptions = LayoutOptions.FillAndExpand}
-                    .Bind(Entry.TextProperty, nameof(ViewModel.ServerUrl)),
-                new Label {Text = "Username"},
-
-                new Entry{HorizontalOptions = LayoutOptions.FillAndExpand}
-                    .Bind(Entry.TextProperty, nameof(ViewModel.Username)),
-                new Label {Text = "Password"},
-
-                new Entry{HorizontalOptions = LayoutOptions.FillAndExpand}
-                    .Bind(Entry.TextProperty, nameof(ViewModel.Password)),
-
-                new Button{Text = "Login", HorizontalOptions = LayoutOptions.Center}
-                    .Bind(Button.CommandProperty, nameof(ViewModel.LoginCommand)),
-
-                new Label{HorizontalOptions = LayoutOptions.Center}
-                    .Bind(Label.TextProperty, nameof(ViewModel.ErrorMessage))
+                RowSpacing = 25,
+                Padding = Device.RuntimePlatform switch
+                {
+                    Device.iOS => new Thickness(30, 60, 30, 30),
+                    _ => new Thickness(30)
+                },
+                RowDefinitions = GridRowsColumns.Rows.Define(
+                    (Row.Server, GridRowsColumns.Auto),
+                    (Row.Username, GridRowsColumns.Auto),
+                    (Row.Password, GridRowsColumns.Auto),
+                    (Row.LoginButton, GridRowsColumns.Auto),
+                    (Row.ErrorMessage, GridRowsColumns.Auto)
+                ),
+                Children =
+                {
+                    new VerticalStackLayout
+                    {
+                        new Label { Text = "Server URL" },
+                        new Entry()
+                            .FillExpandHorizontal()
+                            .Bind(Entry.TextProperty, nameof(ViewModel.ServerUrl)),
+                    }.Row(Row.Server),
+                    new VerticalStackLayout
+                    {
+                        new Label { Text = "Username" },
+                        new Entry()
+                            .FillExpandHorizontal()
+                            .Bind(Entry.TextProperty, nameof(ViewModel.Username)),
+                    }.Row(Row.Username),
+                    new VerticalStackLayout
+                    {
+                        new Label { Text = "Password" },
+                        new Entry()
+                            .FillExpandHorizontal()
+                            .Bind(Entry.TextProperty, nameof(ViewModel.Password)),
+                    }.Row(Row.Password),
+                    new VerticalStackLayout
+                    {
+                        new Button { Text = "Login" }
+                            .CenterHorizontal()
+                            .Bind(Button.CommandProperty, nameof(ViewModel.LoginCommand)),
+                    }.Row(Row.LoginButton),
+                    new VerticalStackLayout
+                    {
+                        new Label()
+                            .CenterHorizontal()
+                            .Bind(Label.TextProperty, nameof(ViewModel.ErrorMessage))
+                    }.Row(Row.ErrorMessage)
+                }
             }
         };
+    }
+
+    private enum Row
+    {
+        Server,
+        Username,
+        Password,
+        LoginButton,
+        ErrorMessage
     }
 }
