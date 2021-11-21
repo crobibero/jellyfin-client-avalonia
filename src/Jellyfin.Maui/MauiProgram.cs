8ffe8f70b3044cc6397c01d3689d82ad68461597
Cody Robibero <cody@robibe.ro>
2021-11-21 13:37:16 -0700

    Add Quicksand font

--- src/Jellyfin.Maui/MauiProgram.cs
+++ src/Jellyfin.Maui/MauiProgram.cs
@@ -30,6 +30,11 @@ public static class MauiProgram
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("Quicksand-Bold.ttf", "QuicksandBold");
+                fonts.AddFont("Quicksand-Light.ttf", "QuicksandLight");
+                fonts.AddFont("Quicksand-Medium.ttf", "QuicksandMedium");
+                fonts.AddFont("Quicksand-Regular.ttf", "QuicksandRegular");
+                fonts.AddFont("Quicksand-SemiBold.ttf", "QuicksandSemiBold");
             });
 
         builder.Services.AddPages();
